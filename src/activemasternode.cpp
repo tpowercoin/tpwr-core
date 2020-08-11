42003c6f00b415830e081514de32f198bcda934d
nagrath <wwbn@gmx.net>
2020-08-11 19:17:52 +0200

    fix some typo's

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -12,7 +12,7 @@
 #include "spork.h"
 
 //
-// Bootup the Masternode, look for a 35000 TPWR input and register on the network
+// Bootup the Masternode, look for a 1777 TPWR input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
